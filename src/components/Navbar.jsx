e7f797f33997441b047ecfbae6016854111284b4
John Doe <johndoe@example.com>
2024-10-14 07:18:35 -0700

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -68,10 +68,12 @@ const Navbar = () => {
             </li>
           ))}
 
-          <a className=" relative top-5 left-[40%] text-lg bg-yellow-500 text-black px-4 py-2 rounded hover:bg-yellow-400">
+          <a className=" relative top-5 left-[41%] text-lg bg-yellow-500 text-black px-4 py-2 rounded hover:bg-yellow-400">
           Download CV
         </a>
+<div className="bg-red-400">
 
+</div>
           </div>
         )}
       </div>
